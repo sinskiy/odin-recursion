31535bf9e32d0c5e63606e46299f92500081e1a7
sinskiy <sinskiy@proton.me>
2024-06-28 21:53:19 +0300

    feat:  implement mergeSort

--- index.js
+++ index.js
@@ -14,3 +14,25 @@ function fibsIter(array, stop) {
 function fibsRec(n) {
   return fibsIter([0, 1], n);
 }
+
+function mergeSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+  const middle = Math.floor(arr.length / 2);
+  const leftArr = mergeSort(arr.slice(0, middle));
+  const rightArr = mergeSort(arr.slice(middle));
+  return merge(mergeSort(leftArr), mergeSort(rightArr));
+}
+
+function merge(leftArr, rightArr) {
+  const sortedArr = [];
+  while (leftArr.length && rightArr.length) {
+    if (leftArr[0] <= rightArr[0]) {
+      sortedArr.push(leftArr.shift());
+    } else {
+      sortedArr.push(rightArr.shift());
+    }
+  }
+  return [...sortedArr, ...leftArr, ...rightArr];
+}
